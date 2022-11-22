49002b7a72f54990b6b72158f2d8c262e6610ee0
Bassam Meshaal <bmeshal@gmail.com>
2022-11-22 13:39:38 -0500

    WIP - still working on state management

--- src/StateMgtDemo.js
+++ src/StateMgtDemo.js
@@ -2,20 +2,33 @@
 
 // https://beta.reactjs.org/learn/managing-state
 
-function StateMgtDemo(){
+import {useEffect, useState} from "react";
 
+function StateMgtDemo(){
+    const [taskText, setTaskText] = useState('');
+
+    useEffect( () => {
+        console.log("init effect....")
+    }, [])
+    useEffect( () => {
+        console.log("taskText is changing..." + taskText)
+    }, [taskText])
 
     return (
         <>
             <h1>State Management</h1>
-            {/*<AddTask*/}
-            {/*    onAddTask={handleAddTask}*/}
-            {/*/>*/}
-            {/*<TaskList*/}
-            {/*    tasks={tasks}*/}
-            {/*    onChangeTask={handleChangeTask}*/}
-            {/*    onDeleteTask={handleDeleteTask}*/}
-            {/*/>*/}
+            <input
+                placeholder="Add task"
+                value={taskText}
+                onChange={e => { setTaskText(e.target.value); console.log(taskText)}}
+            />
+            <button onClick={() => {console.log("text...")}}>Add</button>
+            <ul>
+                <li>
+                    {taskText}
+                    <button onClick={() => console.log("deleting")}>Delete</button>
+                </li>
+            </ul>
         </>
     );
 }
