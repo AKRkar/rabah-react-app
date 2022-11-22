0ae9784a26719676f4cfa729608eeac6867ad360
Bassam Meshaal <bmeshal@gmail.com>
2022-11-22 12:43:56 -0500

    1st successful router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import React from "react";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
-      <>
-          <span>Thing to learn about React:</span>
-        <ul>
-            <li>React Router</li>
-
-        </ul>
-      </>
+          <BrowserRouter>
+              <Routes>
+                  <Route path="/" >
+                    <Route index element={<Home />} />
+                    <Route path="route" element={<Routing />} />
+                  </Route>
+              </Routes>
+          </BrowserRouter>
   );
+}
+
+function Home() {
+    return (
+        <>
+            <h2>Things to learn:</h2>
+            <ul>
+                <li> <a href="/route" > React Router </a></li>
+            </ul>
+        </>
+    );
+
+}
+function Routing() {
+    return (
+        <>
+            <ul>
+                <li> React Routering</li>
+            </ul>
+        </>
+    );
+
 }
 
 export default App;
