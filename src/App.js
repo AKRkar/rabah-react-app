dd0044ecc08f2a3dd39b4fc40e55804a12c773bf
Bassam Meshaal <bmeshal@gmail.com>
2022-11-22 21:47:37 -0500

    show branch diversion

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Routing from "./Routing";
 
 
-function App() {
+function App() { // show git branching...
   return (
           <BrowserRouter>
               <Routes>
