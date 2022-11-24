ee3b33c06f74988a4ede7bd9f6cee13ddf11fc79
Bassam Meshaal <bmeshal@gmail.com>
2022-11-24 12:38:15 -0500

    handle state mgt within same component

--- src/StateMgtDemo.js
+++ src/StateMgtDemo.js
@@ -1,11 +1,13 @@
 
-
 // https://beta.reactjs.org/learn/managing-state
 
 import {useEffect, useState} from "react";
 
 function StateMgtDemo(){
     const [taskText, setTaskText] = useState('');
+    const [taskList, setTaskList] = useState([]);
+    const [nextTaskId, setNextTaskId] = useState(0);
+
 
     useEffect( () => {
         console.log("init effect....")
@@ -20,14 +22,21 @@ function StateMgtDemo(){
             <input
                 placeholder="Add task"
                 value={taskText}
-                onChange={e => { setTaskText(e.target.value); console.log(taskText)}}
+                onChange={e => { setTaskText(e.target.value);}}
             />
-            <button onClick={() => {console.log("text...")}}>Add</button>
+            <button onClick={() => {
+                setTaskList([...taskList, { id: nextTaskId, name: taskText }]);
+                setNextTaskId(nextTaskId + 1);
+            }}>
+                Add
+            </button>
             <ul>
-                <li>
-                    {taskText}
-                    <button onClick={() => console.log("deleting")}>Delete</button>
-                </li>
+                {taskList.map(t => {
+                    return (<li key={nextTaskId}>
+                        {t.name}
+                        <button onClick={() => console.log("deleting")}>Delete</button>
+                    </li>)
+                })}
             </ul>
         </>
     );
