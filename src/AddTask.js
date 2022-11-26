a0ddd9a965efe5220b85b1b2105b8de019b36164
Bassam Meshaal <bmeshal@gmail.com>
2022-11-26 14:43:30 -0500

    WIP - still working on this one

--- src/AddTask.js
+++ src/AddTask.js
@@ -1,16 +1,29 @@
 
+import {useContext, useEffect, useState} from 'react';
+import { TaskContext } from "./TaskContext"
 
-export default function AddTask ({taskText, setTaskText, taskList, setTaskList}){
+export default function AddTask ({taskList, setTaskList}){
+    const taskContext = useContext(TaskContext);
+    const [tt, setTt] = useState('');
+
+    useEffect( () => {
+        console.log(taskContext)
+        setTt(taskContext.taskText)
+    })
+
+    // useEffect( () => {
+    //     setTt(taskContext.taskText);
+    // }, [tt])
 
     return(
         <>
             <input
                 placeholder="Add task"
-                value={taskText}
-                onChange={e => { setTaskText(e.target.value);}}
+                value={tt}
+                onChange={e => { taskContext.setTaskText(e.target.value);}}
             />
             <button onClick={() => {
-                setTaskList([...taskList, { id: taskList.length, name: taskText }]);
+                setTaskList([...taskList, { id: taskList.length, name: taskContext.taskText }]);
             }}>
                 Add
             </button>
--- src/StateMgtDemo.js
+++ src/StateMgtDemo.js
@@ -1,26 +1,33 @@
 
 // https://beta.reactjs.org/learn/managing-state
 
-import {useEffect, useState} from "react";
+import {useEffect, useState, useContext} from "react";
 import TaskList from "./TaskList";
 import AddTask from "./AddTask";
+import { TaskContext } from "./TaskContext"
 
 function StateMgtDemo(){
+    const taskContext = useContext(TaskContext);
     const [taskText, setTaskText] = useState('');
     const [taskList, setTaskList] = useState([]);
 
     useEffect( () => {
-        console.log("init effect....")
+        // console.log("init effect....")
+        // taskContext.setTaskText = setTaskText;
     }, [])
     useEffect( () => {
-        console.log("taskText is changing..." + taskText)
+        console.log("taskText is changing: " + taskText)
+        taskContext.taskText = taskText;
+        if(taskText === '') taskContext.setTaskText = setTaskText;
     }, [taskText])
 
     return (
         <>
             <h1>State Management</h1>
-            <AddTask taskText={taskText} setTaskText={setTaskText} taskList={taskList} setTaskList={setTaskList}/>
-            <TaskList list={taskList} />
+            <TaskContext.Provider value={taskContext}>
+                <AddTask taskList={taskList} setTaskList={setTaskList}/>
+                <TaskList list={taskList} />
+            </TaskContext.Provider>
 
         </>
     );
