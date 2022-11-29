a9587066aa41715a9c3d904f2e05632b183519de
AKR <abdulkarimr71@gmail.com>
2022-11-29 18:45:25 +0200

    clean up

--- src/TaskList.js
+++ src/TaskList.js
@@ -1,13 +1,17 @@
 
 function TaskList({list}){
-    const printLi = (t) => {
+    const printLi = (task) => {
         return (
-                <li key={t.id}>
-                    {t.name}
-                    <button onClick={() => console.log("editing")}>edit</button>
-                    <button onClick={() => console.log("deleting")}>Delete</button>
-                </li>
+            <Task  t={task} />  
         )
+    }
+    function Task ({t}){
+        return (
+            <li key={t.id}>
+                    {t.name}
+                    <button onClick={() => console.log("deleting")}>Delete</button>
+            </li>
+        );
     }
     const taskList = list.map((t) => printLi(t));
     return (
